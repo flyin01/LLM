3ca21e043306adfe2005e92c235d1864625816fb
flyin01 <globaljm@hotmail.com>
2024-02-18 20:04:26 +0100

    clean up

--- trav/src/data_reader.py
+++ trav/src/data_reader.py
@@ -3,9 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 
-### Read in one sample text file 
-
-# absolute_file_path = "C:/repo/LLM/trav/src/data/20230617_V75_1.txt"
+### Define path of data
 
 # Get the directory where this script is located
 script_directory: str = os.getcwd()
@@ -16,10 +14,17 @@ relative_file_path: str = "trav/src/data/"
 # Construct the absolute file path
 absolute_file_path: str = os.path.join(script_directory, relative_file_path).replace("\\", "/")
 
+### Read in one sample .txt file - Commented out!
+
+# absolute_file_path = "C:/repo/LLM/trav/src/data/20230617_V75_1.txt"
+
 # Sample file
 sample_file_name: str = "20230617_V75_1.txt"
 
-# Initialize an empty list to store the file contents
+# Check path
+print(absolute_file_path + sample_file_name)
+
+# Initialize an empty list to store the file content
 file_contents_list = []
 
 # Read the file line by line and append each line to the list
@@ -27,12 +32,17 @@ with open(absolute_file_path + sample_file_name, "r", encoding="utf-8") as file:
     for line in file:
         file_contents_list.append(line.strip())  # Remove leading/trailing whitespace
 
-# Check resulting list
-print(file_contents_list[0:4])
+# Check resulting list, txt file should be read in line by line.
+for line_no, line in enumerate(file_contents_list[:12]):
+    print(line_no, line)
+
+for line_no, line in enumerate(file_contents_list[-12:]):
+    print(line_no, line)
+
+print(len(file_contents_list)) # Should match the rows of the .txt file
 
 
-
-### Read in all files
+### Read in all .txt files
 
 # initialize empty dict to store text corpus
 file_contents_dict = {}
@@ -43,16 +53,26 @@ print(f"Absolute file path: {absolute_file_path}")
 for filename in os.listdir(absolute_file_path):
     if filename.endswith(".txt"):
         file_path = os.path.join(absolute_file_path, filename)
-        with open(absolute_file_path, "r", encoding="utf-8") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             # read the entire file content as a string
-            file_contents = file.read()
-            # add the file name (withouth extension) as key
-            # and the file conent as the value
-            file_contents_dict[filename[:-4]] = file_contents
+            file_contents = file.readlines()
+            # get the first sentence (header) and the rest of the content (corpus)
+            first_sentence = file_contents[0].strip()
+            corpus = ''.join(file_contents[1:]).strip()
+            # add file name (without extension) as key and file content as value
+            file_contents_dict[filename[:-4]] = {"File Name": filename[:-4], "File Header": first_sentence, "File Content": corpus}
 
-# print the resulting dictionary
-for filename,content in file_contents_dict.items():
-    print(f"File: {filename}, Content: {content[:50]}...")
+# Check the resulting dictionary and check results
+print(f"Number of dict entries: {len(file_contents_dict)}")
 
-print(file_contents_dict)
+# Check structure and content 
+for key, value in file_contents_dict.items():
+    print(f"File Name: {value['File Name']}\nFile Header: {value['File Header']}\nFirst 100 characters of content: {value['File Content'][:100]}\n\n")
 
+# Convert to pandas df
+pdf = pd.DataFrame.from_dict(file_contents_dict, orient='index')
+pdf.reset_index(inplace=True)
+pdf = pdf.drop(columns=['index'])
+pdf.columns = ["File Name", "File Header", "File Content"]
+print(f"Shape of pdf: {pdf.shape}")
+pdf.head()
